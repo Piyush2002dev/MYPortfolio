8ca228a0b7b857080fbd940e2f9537ab96d8da3c
Piyush Das <82888840+Piyush2002dev@users.noreply.github.com>
2024-06-14 01:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Full Web Stack Developer", "Web Designer", "Photo / Video Editor"],
+    strings: ["Full Stack Web Developer", "Web Designer", "Photo / Video Editor"],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 1000,
