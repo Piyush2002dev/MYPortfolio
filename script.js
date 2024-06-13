fd5891fdfc831bd3d21f8aad6647010a79c8d476
Piyush Das <82888840+Piyush2002dev@users.noreply.github.com>
2024-06-13 20:26:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Full Stack Developer", "Web Designer", "Photo - Video Editor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
